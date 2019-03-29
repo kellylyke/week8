7abedf5f85bf65f2b1a1365da6a0e28f31b81972
Kelly Lyke <klyke@madisoncollege.edu>
2019-03-29 16:11:14 -0500

    finished act 2

--- soapservice/src/test/java/com/daohostings/temperatureconversions/TemperatureConversionsTest.java
+++ soapservice/src/test/java/com/daohostings/temperatureconversions/TemperatureConversionsTest.java
@@ -6,6 +6,12 @@ import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
+/**
+ * Activity 2 - week 8
+ * @author klyke
+ */
+
+
 public class TemperatureConversionsTest {
 
     @Test
@@ -17,10 +23,37 @@ public class TemperatureConversionsTest {
     }
 
     @Test
-    public void getTemperatureConversionsSoap12() {
+    public void getCelsiusToFahrenheitSuccess() {
         TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
         BigDecimal result = service.celsiusToFahrenheit(BigDecimal.valueOf(0));
         assertEquals(BigDecimal.valueOf(32), result);
+    }
+
+
+    @Test
+    public void getWindChillInFahrenheitSuccess() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInFahrenheit(BigDecimal.valueOf(32), BigDecimal.valueOf(1) );
+        assertEquals(BigDecimal.valueOf(32.46872), result);
+
+
+    }
+
+    @Test
+    public void getWindChillInCelsiusSuccess() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInCelsius(BigDecimal.valueOf(0), BigDecimal.valueOf(5) );
+        assertEquals(BigDecimal.valueOf(-4.09), result);
+
+    }
+
+    @Test
+    public void getFahrenheitToCelsiusSuccess() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.fahrenheitToCelsius(BigDecimal.valueOf(32));
+        assertEquals(BigDecimal.valueOf(0), result);
+
+
     }
 
     @Test
